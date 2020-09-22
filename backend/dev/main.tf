abae36cc93224cc1b1e95eddc7f9bcdabc74903a
danwiltshire <me@danielwiltshire.co.uk>
2020-09-22 21:39:39 +0100

    frontend cloudfront dist

--- backend/dev/main.tf
+++ backend/dev/main.tf
@@ -103,6 +103,50 @@ resource "aws_s3_bucket" "danflix-storage-frontend" {
   }
 }
 
+resource "aws_cloudfront_distribution" "danflix-cloudfront-frontend" {
+  origin {
+    domain_name = aws_s3_bucket.danflix-storage-frontend.bucket_regional_domain_name
+    origin_id   = "danflix-${var.environment}-storage-frontend"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "danflix-${var.environment}-storage-frontend"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Environment = "${var.environment}"
+  }
+}
+
 
 # TODO: Need a better way of managing Lambda function code
 data "archive_file" "danflix-lambda-code-get-presignedurl" {
