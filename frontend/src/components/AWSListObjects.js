3ced2e2f68f47140e5b289fa92c984fe13ac1ac6
danwiltshire <me@danielwiltshire.co.uk>
2020-09-24 17:39:14 +0100

    faulty

--- frontend/src/components/AWSListObjects.js
+++ frontend/src/components/AWSListObjects.js
@@ -1,42 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
-
-function GetData() {
-    const [data, setData] = useState({ Contents: [] });
-   
-    useEffect(() => {
-        const fetchData = async () => {
-          const result = await axios(
-            'https://62vdqtbnec.execute-api.eu-west-2.amazonaws.com/danflix-listObjects',
-          );
-            console.log(result.data)
-          setData(result.data);
-        };
-     
-        fetchData();
-      }, []);
-   
-    return (
-      <ul>
-        {data.Contents.map(item => (
-          <li key={item.Key}>
-            <a href={item.Key}>{item.Key}</a>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-   
 
 const AWSListObjects = () => {
-    const { isAuthenticated } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
+  const [posts, setPosts] = useState(null);
 
-    return (
-        !isAuthenticated && (
-            GetData()
-        )
-    )
-}
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          audience: 'https://7njjpyiofg.execute-api.eu-west-2.amazonaws.com/'
+        });
+        const response = await fetch('https://7njjpyiofg.execute-api.eu-west-2.amazonaws.com/listobjects', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setPosts(await response.json());
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently]);
 
-export default AWSListObjects
+  if (!posts) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <ul>
+      {posts.map((post, index) => {
+        return <li key={index}>{post}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default AWSListObjects;
