de6530cb05b06d39c98eb7e317f19ea1c948d2ca
danwiltshire <me@danielwiltshire.co.uk>
2020-09-25 19:44:11 +0100

    filtered listObjects

--- backend/main.tf
+++ backend/main.tf
@@ -297,32 +297,26 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 exports.handler = async (event, context) => {
-
-    var params = { 
-        Bucket: '${aws_s3_bucket.danflix-storage-media.id}'
+    
+    const res = await s3.listObjectsV2({
+      Bucket: '${aws_s3_bucket.danflix-storage-media.id}'
+    }).promise();
+    
+    const items = res.Contents.filter(item => item.Key.endsWith('.m3u8'));
+    
+    if ( items ) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(items)
+        }
+    } else {
+        return {
+            statusCode: 404,
+        }
     }
-
-    console.log("Checkpoint 1");
-
-    let s3Objects
-
-    try {
-       s3Objects = await s3.listObjectsV2(params).promise();
-       console.log(s3Objects)
-    } catch (e) {
-       console.log(e)
-    }
-
-
-    console.log("Checkpoint 2");
-
-    // Assuming you're using API Gateway
-    return {
-        statusCode: 200,
-        body: JSON.stringify(s3Objects || {message: 'No objects found in s3 bucket'})
-    }
-
+    
 };
+
 EOF
     filename = "index.js"
   }
