3151b6ac1e0bb9f8b47aca44eba4bcd070811f13
danwiltshire <me@danielwiltshire.co.uk>
2020-09-25 13:57:02 +0100

    return to correct URL after logout

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -6,7 +6,7 @@ const LogoutButton = () => {
 
     return (
         isAuthenticated && (
-            <button onClick={() => logout()}>
+            <button onClick={() => logout({ returnTo: window.location.origin })}>
                 Log Out
             </button>
         )
