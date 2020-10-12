0a336c91d877289689b244ee4c7b9188fab6844d
danwiltshire <me@danielwiltshire.co.uk>
2020-10-12 21:04:01 +0100

    functional test

--- backend/test/integration/default/controls/authentication.rb
+++ backend/test/integration/default/controls/authentication.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-control "url_check" do
-  describe http('https://dwlab.eu.auth0.com/oauth/token',
-                method: 'POST',
-                headers: {'Content-Type' => 'application/json'},
-                data: '{"client_id":"x","client_secret":"x","audience":"x","grant_type":"client_credentials"}') do
-    its('status') { should cmp 200 }
-    its('body') { should match '\"token_type\":\"Bearer\"' }
+["listobjects", "presignedurl"].each do |path|
+  control "Unauthenticated call to /#{path}" do
+    describe http("#{attribute("api_invoke_url")}#{path}") do
+      its('status') { should cmp 401 }
+      its('body') { should match '{"message":"Unauthorized"}' }
+    end
   end
 end
