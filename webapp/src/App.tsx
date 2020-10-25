0bfd686097a57edf9b078a2001657820d450cfd3
danwiltshire <me@danielwiltshire.co.uk>
2020-10-25 22:14:18 +0000

    sample map

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Button } from './components/Button'
 import { Counter } from './components/Counter'
+import { List } from './components/List'
 import { Logo } from './components/Logo'
 
 export const App: React.FC = () => {
+
+  const listItems = [
+    { text: "A link to BBC", link: "https://www.bbc.co.uk/" },
+    { text: "A link to Google", link: "https://www.google.co.uk/" },
+    { text: "Just a list item" },
+    { text: "Another list item" },
+    { text: "A link to nothing", link: "http://localhost:3000" }
+  ]
+
   return (
     <div>
       <Counter>
@@ -18,6 +28,11 @@ export const App: React.FC = () => {
       <Logo
         width='auto'
         height='42px'
+      />
+      <List
+        items={
+          listItems
+        }
       />
     </div>
   );
